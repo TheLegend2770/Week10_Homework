42324b12d2e52a4ea5b90d62cfc7c5f3dc60cdbc
TheLegend2770 <48347229+TheLegend2770@users.noreply.github.com>
2022-10-05 17:08:17 +1100

    Add files via upload

--- cheese_award_table.js
+++ cheese_award_table.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/TheLegend2770/Week10_Homework/main/json/Week_10_hw.vg.json";
+var vg_1 = "json/Week_10_hw.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
